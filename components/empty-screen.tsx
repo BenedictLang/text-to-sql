02b44137298911603da76ae51a6972a077ca9e90
BenedictLang <benedict.lang@outlook.de>
2024-09-14 06:33:18 +0200

    feat(): set empty screen text

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -9,28 +9,20 @@ export function EmptyScreen() {
     <div className="mx-auto max-w-2xl px-4">
       <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
         <h1 className="text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
+          Welcome to your triage assistent!
         </h1>
         <p className="leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink>, the{' '}
-          <ExternalLink href="https://sdk.vercel.ai">
-            Vercel AI SDK
-          </ExternalLink>
-          , and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
-          </ExternalLink>
-          .
+          Connect a database and ask me questions about it.
+        </p>
+        <p className="mt-4 italic leading-normal text-muted-foreground">
+          This chatbot is part of a text-to-SQL transformation test focused on
+          log analysis under the topic 'Leveraging Large Language Models
+          for Modern Security Incident Management,' intended for scientific purposes
+          and not for production.
         </p>
         <p className="leading-normal text-muted-foreground">
-          It uses{' '}
-          <ExternalLink href="https://vercel.com/blog/ai-sdk-3-generative-ui">
-            React Server Components
-          </ExternalLink>{' '}
-          to combine text with generative UI as output of the LLM. The UI state
-          is synced through the SDK so the model is aware of your interactions
-          as they happen.
+          You can find the projects sourcecode on
+          {' '}<ExternalLink href="https://github.com/BenedictLang/text-to-sql">GitHub</ExternalLink>{' '}.
         </p>
       </div>
     </div>
